9276524428c50841a95740db7b8fa1911f6f3bce
Lane Henslee <huncho@hygo.ai>
2024-11-10 03:11:57 -0800

    better background colors

--- src/decorationSelector.ts
+++ src/decorationSelector.ts
@@ -2,36 +2,31 @@ import * as vscode from "vscode";
 
 const BACKGROUND_COLORS = [
   "rgba(255, 255, 255, 0.2)", // White (For outer group)
-  "rgba(255, 0, 0, 0.5)", // Red
-  "rgba(0, 255, 0, 0.5)", // Lime
-  "rgba(0, 0, 255, 0.5)", // Blue
-  "rgba(255, 255, 0, 0.5)", // Yellow
-  "rgba(255, 0, 255, 0.5)", // Magenta
-  "rgba(0, 255, 255, 0.5)", // Cyan
-  "rgba(255, 165, 0, 0.5)", // Orange
-  "rgba(255, 20, 147, 0.5)", // Deep Pink
-  "rgba(75, 0, 130, 0.5)", // Indigo
-  "rgba(238, 130, 238, 0.5)", // Violet
-  "rgba(138, 43, 226, 0.5)", // Blue Violet
-  "rgba(95, 158, 160, 0.5)", // Cadet Blue
-  "rgba(127, 255, 0, 0.5)", // Chartreuse
-  "rgba(210, 105, 30, 0.5)", // Chocolate
-  "rgba(255, 127, 80, 0.5)", // Coral
-  "rgba(100, 149, 237, 0.5)", // Cornflower Blue
-  "rgba(220, 20, 60, 0.5)", // Crimson
-  "rgba(0, 206, 209, 0.5)", // Dark Turquoise
-  "rgba(148, 0, 211, 0.5)", // Dark Violet
+  "rgba(255, 99, 71, 0.5)", // Light Coral
+  "rgba(255, 140, 0, 0.5)", // Dark Orange
   "rgba(255, 215, 0, 0.5)", // Gold
-  "rgba(173, 255, 47, 0.5)", // Green Yellow
-  "rgba(255, 105, 180, 0.5)", // Hot Pink
-  "rgba(205, 92, 92, 0.5)", // Indian Red
   "rgba(240, 230, 140, 0.5)", // Khaki
-  "rgba(230, 230, 250, 0.5)", // Lavender
-  "rgba(124, 252, 0, 0.5)", // Lawn Green
-  "rgba(32, 178, 170, 0.5)", // Light Sea Green
+  "rgba(144, 238, 144, 0.5)", // Light Green
+  "rgba(152, 251, 152, 0.5)", // Pale Green
+  "rgba(60, 179, 113, 0.5)", // Medium Sea Green
   "rgba(135, 206, 250, 0.5)", // Light Sky Blue
-  "rgba(255, 99, 71, 0.5)", // Tomato
-  "rgba(50, 205, 50, 0.5)", // Lime Green
+  "rgba(173, 216, 230, 0.5)", // Light Blue
+  "rgba(135, 206, 235, 0.5)", // Sky Blue
+  "rgba(221, 160, 221, 0.5)", // Plum
+  "rgba(216, 191, 216, 0.5)", // Thistle
+  "rgba(240, 128, 128, 0.5)", // Light Salmon
+  "rgba(245, 222, 179, 0.5)", // Wheat
+  "rgba(255, 192, 203, 0.5)", // Pink
+  "rgba(255, 182, 193, 0.5)", // Light Pink
+  "rgba(238, 232, 170, 0.5)", // Pale Goldenrod
+  "rgba(255, 228, 196, 0.5)", // Bisque
+  "rgba(255, 218, 185, 0.5)", // Peach Puff
+  "rgba(175, 238, 238, 0.5)", // Pale Turquoise
+  "rgba(224, 255, 255, 0.5)", // Light Cyan
+  "rgba(240, 255, 240, 0.5)", // Honeydew
+  "rgba(250, 250, 210, 0.5)", // Light Goldenrod Yellow
+  "rgba(245, 245, 220, 0.5)", // Beige
+  "rgba(255, 250, 205, 0.5)", // Lemon Chiffon
 ];
 
 class DecorationSelector {
@@ -44,6 +39,7 @@ class DecorationSelector {
         vscode.window.createTextEditorDecorationType({
           backgroundColor: color,
           color: "black",
+          fontWeight: "900",
         })
       );
     }
